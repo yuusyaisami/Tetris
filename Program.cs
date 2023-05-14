0098f205c2b63fabbbd12fd102ee4810fc174327
oreo12411 <110176625+yuusyaisami@users.noreply.github.com>
2023-05-14 20:06:30 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Program
+    {
+        static int[][] Map = {  new int[]{ 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999,   0,   0,   0,   0,   0,   0,   0,   0,   0, 999},
+                                new int[]{ 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999}};
+        static int StartGame;
+        static int CreateBlock;
+        static void Main(string[] args)
+        {
+            initializ();
+            for (; ; )
+            {
+                if (CreateBlock == 0)
+                {
+                    CreateBlock = 1;
+                    CreateNewBlock();
+                }
+            }
+            Console.Clear();
+        }
+        public static void initializ()
+        {
+            
+        }
+        public static void FallBlock()
+        {
+
+        }
+        public static bool CheckDoMove()
+        {
+            return true;
+        }
+        public static void ControlBlock()
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.WriteLine("Key: " + keyInfo.Key + ", Char: " + keyInfo.KeyChar + ", Modifiers: " + keyInfo.Modifiers);
+            if (keyInfo.Key == ConsoleKey.W)
+            {
+
+            }
+        }
+        public static void CreateNewBlock() 
+        {
+
+        }
+        public static void ClearLineBlock()
+        {
+
+        }
+    }
+}
